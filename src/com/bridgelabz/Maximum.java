52598506fc8d79cb4beae2e17e4c64345c37e976
Annu <annusinghraj1998@gmail.com>
2022-06-22 15:23:35 +0530

    ADDED : Refactored all three method to one generic method

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -1,42 +1,23 @@
 package com.bridgelabz;
 
-import java.util.Scanner;
-
 public class Maximum {
-    public static void main(String[] args) {
-        System.out.println("Welcome to maximum number program !");
-
-        Maximum maximum = new Maximum();
-
-        System.out.println("Maximum string among three string is : " + maximum.compareTo());
-
+    public static <T extends Comparable> T testCase(T a, T b, T c) {
+        T max = a;
+        if (b.compareTo(max) > 0) {
+            max = b;
+        }
+        if (c.compareTo(max) > 0) {
+            max = c;
+        }
+        return max;
     }
 
-    // compareTo method check the maximum number among three numbers and return it :-
-    public String compareTo() {
+    public static void main(String[] args) {
+        System.out.println("Welcome to maximum number program !");
+        System.out.println("Maximum among three integer values is :" + testCase(9, 5, 10));
+        System.out.println("Maximum among three float values is :" + testCase(9.8f, 5.7f, 101.7f));
+        System.out.println("Maximum among three string values is :" + testCase("Annu", "Garima", "Sheetal"));
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter first string");
-        String string1 = scanner.next();
-
-        System.out.println("Enter second string");
-        String string2 = scanner.next();
-
-        System.out.println("Enter third number");
-        String string3 = scanner.next();
-
-        String maximum = string1;
-
-        if(string2.compareTo(maximum) > 0){
-            maximum = string2;
-
-        }
-        if (string3.compareTo(maximum) > 0){
-            maximum = string3;
-
-        }
-        return maximum;
     }
 }
 
