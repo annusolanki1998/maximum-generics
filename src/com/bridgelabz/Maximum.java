eed110a7d95025f5faa4078058b1ebec48f7007f
Annu <annusinghraj1998@gmail.com>
2022-06-22 14:17:40 +0530

    ADDED : Check the maximum among three string value

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -8,34 +8,33 @@ public class Maximum {
 
         Maximum maximum = new Maximum();
 
-        System.out.println("Maximum number among three integer numbers is : " + maximum.compareTo());
+        System.out.println("Maximum string among three string is : " + maximum.compareTo());
 
     }
 
     // compareTo method check the maximum number among three numbers and return it :-
-    public Float compareTo() {
+    public String compareTo() {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter first number");
-        Float number1 = scanner.nextFloat();
+        System.out.println("Enter first string");
+        String string1 = scanner.next();
 
-        System.out.println("Enter second number");
-        Float number2 = scanner.nextFloat();
+        System.out.println("Enter second string");
+        String string2 = scanner.next();
 
         System.out.println("Enter third number");
-        Float number3 = scanner.nextFloat();
+        String string3 = scanner.next();
 
-        Float maximum = number1;
+        String maximum = string1;
 
-        if (number1 > number2 && number1 > number3) {
-            maximum = number1;
+        if(string2.compareTo(maximum) > 0){
+            maximum = string2;
 
-        } else if (number2 > number1 && number2 > number3) {
-            maximum = number2;
+        }
+        if (string3.compareTo(maximum) > 0){
+            maximum = string3;
 
-        } else {
-            maximum = number3;
         }
         return maximum;
     }
