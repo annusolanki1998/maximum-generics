99dfe1a857483305fe8cad9d91f19eced494a6fc
Annu <annusinghraj1998@gmail.com>
2022-06-22 16:32:37 +0530

    ADDED : Printend maximum among integer,float and string value

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -2,17 +2,27 @@ package com.bridgelabz;
 
 import java.util.Arrays;
 
-public class Maximum {
+public class Maximum<T> {
 
-    public static <T> T testCase(T... a){
+    public T testCase(T... a) {
         Arrays.sort(a);
-        return a[a.length -1];
+        T max = a[a.length - 1];
+        printMax(max);
+        return max;
+    }
+
+    private void printMax(T max) {
+        System.out.println(max);
+
     }
 
     public static void main(String[] args) {
-        System.out.println("Maximum among all integer values is :" + testCase(2,9,98,-2,8,7,987,765));
-        System.out.println("Maximum among all float values is :" + testCase(3.9f,89.8f,876.6f,876.6f,999.8f,5.7f,4.5f));
-        System.out.println("Maximum among all string values is :" + testCase("Choti","Garima","Antima","Annu","Rishav"));
+        Maximum<Integer> maximumInteger = new Maximum<>();
+        Maximum<Float> maximumFloat = new Maximum<>();
+        Maximum<String> maximumString = new Maximum<>();
+        maximumInteger.testCase(2, 9, 98, -2, 8, 7, 987, 765);
+        maximumFloat.testCase(3.9f, 89.8f, 876.6f, 876.6f, 999.8f, 5.7f, 4.5f);
+        maximumString.testCase("Choti", "Garima", "Antima", "Annu", "Rishav");
 
     }
 }
