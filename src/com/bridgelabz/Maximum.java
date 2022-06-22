b94e00412abce6354bae3bc31dde08ce1c23e928
Annu <annusinghraj1998@gmail.com>
2022-06-22 15:59:44 +0530

    ADDED : Add more than three parameter and find maximum using sorting

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -1,39 +1,18 @@
 package com.bridgelabz;
 
-class Generic<T extends Comparable> {
-    private T a;
-    private T b;
-    private T c;
-
-    public Generic(T a, T b, T c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-    }
-
-    public T testCase() {
-        T max = a;
-        if (b.compareTo(max) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
-        }
-        return max;
-
-    }
-}
+import java.util.Arrays;
 
 public class Maximum {
 
+    public static <T> T testCase(T... a){
+        Arrays.sort(a);
+        return a[a.length -1];
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to maximum number program !");
-        Generic<Integer> generic1 = new Generic<>(10, 5, 9);
-        Generic<Float> generic2 = new Generic<>(10.6f, 5.7f, 109.8f);
-        Generic<String> generic3 = new Generic<>("Annu", "Choti", "Babli");
-        System.out.println("Maximum among three integer values is :" + generic1.testCase());
-        System.out.println("Maximum among three float values is :" + generic2.testCase());
-        System.out.println("Maximum among three string values is :" + generic3.testCase());
+        System.out.println("Maximum among all integer values is :" + testCase(2,9,98,-2,8,7,987,765));
+        System.out.println("Maximum among all float values is :" + testCase(3.9f,89.8f,876.6f,876.6f,999.8f,5.7f,4.5f));
+        System.out.println("Maximum among all string values is :" + testCase("Choti","Garima","Antima","Annu","Rishav"));
 
     }
 }
